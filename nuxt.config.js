fcfea84573d5b6ac649a6eb0f61c04e0cc75a076
Timotius Nugroho <timotius.nugroho@aruna.id>
2022-12-31 13:02:08 +0700

    add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,15 +4,37 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'poke-nuxt',
+    title: 'Pokemons',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Pokémon! These cute and powerful creatures have captured the hearts of people everywhere.',
+      },
       { name: 'format-detection', content: 'telephone=no' },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: `hhttps://catch-poke-nuxt.vercel.app/`,
+      },
+      { hid: 'og:type', property: 'og:type', content: `website` },
+      { hid: 'og:title', property: 'og:title', content: `Pokemons` },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: `Pokémon! These cute and powerful creatures have captured the hearts of people everywhere.`,
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: `https://catch-poke-nuxt.vercel.app/og-poke.png`,
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
